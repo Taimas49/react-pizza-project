22cddf24c3f298dd4eaaf207d7d4b7352031876a
Taimas <urazgalit@gmail.com>
2020-12-05 12:07:22 +0600

    Used Redux Thunk

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,6 +1,17 @@
-const setPizzas = (arr) => ({
+export const fetchPizzas = () => (dispatch) => {
+    fetch('http://localhost:3001/pizzas')
+    .then((response) => response.json())
+    .then((json) => {
+      dispatch(setPizzas(json));
+    }
+    );
+};
+
+
+
+
+export const setPizzas = (arr) => ({
     type: 'SET_PIZZAS',
     payload: arr
 });
 
-export default setPizzas;
--- src/redux/reducers/pizzas.js
+++ src/redux/reducers/pizzas.js
@@ -9,6 +9,7 @@ const pizzasReducer = (state = initialState, action) => {
         return {
             ...state,
             items: action.payload,
+            isLoaded: true
         };
     }
     return state;
